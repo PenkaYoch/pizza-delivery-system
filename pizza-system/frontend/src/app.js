65dd8847475126dcfbf971294381c2a4fef6476e
penka991234 <penka991234@abv.bg>
2026-05-26 21:54:13 +0300

    Update cache-bursting version

--- pizza-system/frontend/src/app.js
+++ pizza-system/frontend/src/app.js
@@ -1,7 +1,7 @@
-import { createTask, deleteTask, fetchNotifications, fetchTasks, updateTask } from "./services/api.js?v=20260526-last";
-import { getApiUrl } from "./state/config.js?v=20260526-last";
-import { elements } from "./ui/dom.js?v=20260526-last";
-import { render, setMessage } from "./ui/render.js?v=20260526-last";
+import { createTask, deleteTask, fetchNotifications, fetchTasks, updateTask } from "./services/api.js?v=20260526-new";
+import { getApiUrl } from "./state/config.js?v=20260526-new";
+import { elements } from "./ui/dom.js?v=20260526-new";
+import { render, setMessage } from "./ui/render.js?v=20260526-new";
 
 const state = {
   tasks: [],
